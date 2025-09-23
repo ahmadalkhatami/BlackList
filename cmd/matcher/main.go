7a65f5ce6860ded1938a8c5a198a1ed0dc0ce78e
unknown <deji2@gmail.com>
2025-09-23 21:09:56 +0700

    os exit code

--- cmd/matcher/main.go
+++ cmd/matcher/main.go
@@ -4,6 +4,7 @@ import (
 	"BlackListWorker/config"
 	"BlackListWorker/internal/bootstrap"
 	"log"
+	"os"
 )
 
 func main() {
@@ -14,4 +15,9 @@ func main() {
 	if err := app.Start(); err != nil {
 		log.Fatal(err)
 	}
+
+	// Exit Code 0 untuk menandakan proses berhasil
+	// Exit Code 1 untuk menandakan proses gagal
+	// Exit Code 2 (golang memberikan exit code untuk panic)
+	os.Exit(0)
 }
